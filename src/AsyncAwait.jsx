87fd2392872a29161e131b2c6af576ffa84f4de6
dielawn <dielawnmerkill@gmail.com>
2024-02-25 05:01:33 -0700

    fetch using async await

--- src/AsyncAwait.jsx
+++ src/AsyncAwait.jsx
@@ -1,2 +1,26 @@
 import { useState, useEffect } from "react";
+const [data, setData] = useState(null)
+const [loading, setLoading] = useState(true)
+const [error, setError] =  useState(null)
+const [url, setUrl] = useState('https://jsonplaceholder.typicode.com/posts?_limit=')
+const [limit, setLimit] = useState('10')
 
+useEffect(() => {
+    async function getData() {
+        try {
+            const response = await fetch(`${url}${limit}`)
+            if (!response.ok) {
+                throw new Error(`This is an respose error: The status is ${response.status}`)
+            }
+            let actualData = await response.json()
+            setData(actualData)
+            setError(null)
+        } catch(err) {
+            setError(err.message)
+            setData(null)
+        } finally {
+            setLoading(false)
+        }        
+    }
+    getData()
+}, [])
